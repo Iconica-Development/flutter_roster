bdb72520acc45fc69640a9a6215136742f400f34
Bugfix Jacques <jacques@iconica.nl>
2023-05-24 10:21:46 +0200

    Merge remote-tracking branch 'origin/master' into hotfix/packages

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -18,100 +18,88 @@ class RosterDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: RosterWidget(
-          tableDirection: Axis.horizontal,
-          header: Padding(
+      appBar: AppBar(
+        title: const Text('Demo'),
+      ),
+      body: RosterWidget(
+        tableTopPadding: 130,
+        tableDirection: Axis.horizontal,
+        header: Padding(
             padding: EdgeInsets.symmetric(
               horizontal: MediaQuery.of(context).size.width * 0.1,
             ),
-            child: Row(
-              children: [
-                const Text(
-                  teamRoster,
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                const Spacer(),
-                const Text(
-                  personalRoster,
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
+            child: const Padding(
+              padding: EdgeInsets.only(top: 8.0),
+              child: Text('Agenda'),
+            )),
+        blockDimension: 50,
+        highlightToday: false,
+        size: Size(
+          MediaQuery.of(context).size.width,
+          MediaQuery.of(context).size.height * 0.8,
+        ),
+        blocks: [
+          RosterEvent(
+            start: DateTime.now().subtract(const Duration(hours: 3)),
+            end: DateTime.now().add(const Duration(hours: 2)),
           ),
-          childIfEmptyRoster:
-              const Text('no elements', style: TextStyle(color: Colors.red)),
-          blockDimension: 50,
-          highlightToday: false,
-          tableTopPadding: 120,
-          size: Size(
-            MediaQuery.of(context).size.width,
-            MediaQuery.of(context).size.height * 0.8,
+          RosterEvent(
+            start: DateTime.now().subtract(const Duration(hours: 2)),
+            end: DateTime.now().add(const Duration(hours: 3)),
           ),
-          blocks: [
-            RosterEvent(
-              start: DateTime.now().subtract(const Duration(hours: 3)),
-              end: DateTime.now().add(const Duration(hours: 2)),
-            ),
-            RosterEvent(
-              start: DateTime.now().subtract(const Duration(hours: 2)),
-              end: DateTime.now().add(const Duration(hours: 3)),
-            ),
-            RosterEvent(
-              start: DateTime.now().subtract(const Duration(hours: 1)),
-              end: DateTime.now().add(const Duration(hours: 4)),
-            ),
-            RosterEvent(
-              start: DateTime.now().add(const Duration(hours: 3)),
-              end: DateTime.now().add(const Duration(hours: 4)),
-              id: 4,
-              content: const Text('event 4'),
-            ),
-            RosterEvent(
-              start: DateTime.now().add(const Duration(hours: 3)),
-              end: DateTime.now().add(const Duration(hours: 4)),
-              id: 4,
-              content: const Text('event 5'),
-            ),
-            RosterEvent(
-              start: DateTime.now().add(const Duration(hours: 3)),
-              end: DateTime.now().add(const Duration(hours: 4)),
-              id: 4,
-              content: const Text('event 6'),
-            ),
-            RosterEvent(
-              start: DateTime.now().add(const Duration(days: 1)),
-              end: DateTime.now()
-                  .add(const Duration(days: 1))
-                  .add(const Duration(hours: 2)),
-            ),
-            RosterEvent(
-              start: DateTime.now().subtract(const Duration(hours: 2)),
-              end: DateTime.now().add(const Duration(hours: 1)),
-            ),
-            RosterEvent(
-              start: DateTime.now().add(const Duration(days: 2)),
-              end: DateTime.now().add(const Duration(days: 3)),
-            ),
-          ],
-          disabledDates: [
-            // yesterday
-            DateTime.now().subtract(const Duration(days: 1)),
-          ],
-          highlightedDates: [
-            // tomorrow
-            DateTime.now().add(const Duration(days: 1)),
-          ],
-          theme: const RosterTheme(
-            tableTheme: TableTheme(
-              blockPaddingBetween: 10,
-            ),
-            timePickerTheme: DateTimePickerTheme(
-              barTheme: DateTimePickerBarTheme(
-                barColor: Colors.blue,
-                barOpacity: 1,
-                barHeight: 5,
-              ),
+          RosterEvent(
+            start: DateTime.now().subtract(const Duration(hours: 1)),
+            end: DateTime.now().add(const Duration(hours: 4)),
+          ),
+          RosterEvent(
+            start: DateTime.now().add(const Duration(hours: 3)),
+            end: DateTime.now().add(const Duration(hours: 4)),
+            id: 4,
+            content: const Text('event 4'),
+          ),
+          RosterEvent(
+            start: DateTime.now().add(const Duration(hours: 3)),
+            end: DateTime.now().add(const Duration(hours: 4)),
+            id: 4,
+            content: const Text('event 5'),
+          ),
+          RosterEvent(
+            start: DateTime.now().add(const Duration(hours: 3)),
+            end: DateTime.now().add(const Duration(hours: 4)),
+            id: 4,
+            content: const Text('event 6'),
+          ),
+          RosterEvent(
+            start: DateTime.now().add(const Duration(days: 1)),
+            end: DateTime.now()
+                .add(const Duration(days: 1))
+                .add(const Duration(hours: 2)),
+          ),
+          RosterEvent(
+            start: DateTime.now().subtract(const Duration(hours: 2)),
+            end: DateTime.now().add(const Duration(hours: 1)),
+          ),
+          RosterEvent(
+            start: DateTime.now().add(const Duration(days: 2)),
+            end: DateTime.now().add(const Duration(days: 3)),
+          ),
+        ],
+        disabledDates: [
+          // yesterday
+          DateTime.now().subtract(const Duration(days: 1)),
+        ],
+        highlightedDates: [
+          // tomorrow
+          DateTime.now().add(const Duration(days: 1)),
+        ],
+        theme: const RosterTheme(
+          tableTheme: TableTheme(
+            blockPaddingBetween: 10,
+          ),
+          timePickerTheme: DateTimePickerTheme(
+            barTheme: DateTimePickerBarTheme(
+              barOpacity: 1,
+              barHeight: 5,
             ),
           ),
         ),
